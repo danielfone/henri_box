d678a41d9f2ea781dded26bd08c72a549bf23481
Daniel Fone <daniel@fone.net.nz>
2013-01-27 05:36:31 +1300

    Rails 3.2.11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HenriBox::Application.config.secret_token = 'aa0226ad57a39cb13aa5ebb8293c65ada952ea7c59e4099e231ff410a19655c935b2964945d295b143623196b670a8146ce2ed3a7d334d005ee889b35a5eeeb7'
