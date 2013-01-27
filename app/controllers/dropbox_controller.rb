653dc18ee065e91e75a4e4796defdf72058fd2dd
Daniel Fone <daniel@fone.net.nz>
2013-01-27 20:21:45 +1300

    Refactor dropbox controller

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -10,27 +10,34 @@ class DropboxController < ApplicationController
   end
 
   def send_to_dropbox
-    dbsession = DropboxSession.new dropbox_config[:app_key], dropbox_config[:app_secret]
-    session[:dropbox_session] = dbsession.serialize
-
-    redirect_to dbsession.get_authorize_url url_for action: 'callback'
+    session[:dropbox_session] = dropbox_session.serialize
+    redirect_to dropbox_session.get_authorize_url url_for action: 'callback'
   end
 
   def callback
-    dbsession = DropboxSession.deserialize session[:dropbox_session]
-    dbsession.get_access_token
-    session[:dropbox_session] = dbsession.serialize
+    dropbox_session.get_access_token
+    session[:dropbox_session] = dropbox_session.serialize
 
     redirect_to home_path
   end
 
   def info
-    dbsession = DropboxSession.deserialize(session[:dropbox_session])
-    client = DropboxClient.new(dbsession, :app_folder) #raise an exception if session not authorized
+    client = DropboxClient.new(dropbox_session, :app_folder) #raise an exception if session not authorized
     render text: client.account_info.to_yaml, content_type: Mime::TEXT
+  rescue DropboxAuthError
+    render text: 'Not authenticated', content_type: Mime::TEXT
   end
 
   private
+
+    def dropbox_session
+      @dropbox_session ||= if session[:dropbox_session]
+        DropboxSession.deserialize session[:dropbox_session]
+      else
+        raise "Dropbox not configured" unless dropbox_config[:app_key] and dropbox_config[:app_secret]
+        DropboxSession.new dropbox_config[:app_key], dropbox_config[:app_secret]
+      end
+    end
 
     def home_path
       url_for action: 'info'
